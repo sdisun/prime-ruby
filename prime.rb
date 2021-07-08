6c150479bffc8821c97229ffa96d9a9624dfaa8d
Neil <zooming.in@hotmail.com>
2021-07-09 00:47:03 +1000

    updates

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+# A prime number can be divided, without a remainder, only by itself and by 1. 
+
+def prime?(num)
+    if num <= 1
+        return false
+    elsif (2..(num - 1)).each {|n| num % n == 0 ? false : true}
+        true
+    end
+end
